d2fcb2dbff7649b108924a626964cecf0700e631
vedant8 <vedant8basu@gmail.com>
2018-02-10 12:41:46 +0530

    Update sort.cpp
    REmoved the nested loops

--- Bitwise/sort.cpp
+++ Bitwise/sort.cpp
@@ -9,17 +9,17 @@
  
 
 // Count sort of arr[].
-void countSort(ap_int<5>  arr[9][6], int exp)
+void countSort(ap_int<2>  arr[18][6], int exp)
 {
 #pragma HLS PIPELINE II=6
 	// Count[i] array will be counting the number of array values having that 'i' digit at their (exp)th place.
-	ap_int<5>  output[9][6];
+	ap_int<2>  output[18][6];
 	int i, count[2] = {0};
 #pragma HLS ARRAY_PARTITION variable=output complete dim=0
 #pragma HLS ARRAY_PARTITION variable=count complete dim=0
 
 	// Count the number of times each digit occurred at (exp)th place in every input.
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 18; i++)
      {
 		#pragma HLS unroll
 		count[arr[i][exp]]++;
@@ -28,34 +28,41 @@ void countSort(ap_int<5>  arr[9][6], int exp)
 
 		count[1] += count[0];
 
-	// Inserting values according to the digit '(arr[i] / exp) % 10' fetched into count[(arr[i] / exp) % 10].
-	for (i = 9 - 1; i >= 0; i--)
+	// Inserting values according to the digit '(arr[i] / exp) % 18' fetched into count[(arr[i] / exp) % 18].
+	for (i = 18 - 1; i >= 0; i--)
 	{
       #pragma HLS unroll
-		for(int j=0;j<6;j++)
-	   {
-        #pragma HLS unroll
-		output[count[arr[i][exp]] - 1][j] = arr[i][j];
-	  }
+
+		output[count[arr[i][exp]] - 1][0] = arr[i][0];
+		output[count[arr[i][exp]] - 1][1] = arr[i][1];
+		output[count[arr[i][exp]] - 1][2] = arr[i][2];
+		output[count[arr[i][exp]] - 1][3] = arr[i][3];
+		output[count[arr[i][exp]] - 1][4] = arr[i][4];
+		output[count[arr[i][exp]] - 1][5] = arr[i][5];
+
 		count[arr[i][exp]]--;
 
 	}
 
 	// Assigning the result to the arr pointer of main().
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 18; i++)
 		{
 		   #pragma HLS unroll
-			for(int j=0;j<6;j++)
-			{
-           #pragma HLS unroll
-			arr[i][j]=output[i][j];
-			}
+
+			arr[i][0]=output[i][0];
+			arr[i][1]=output[i][1];
+			arr[i][2]=output[i][2];
+			arr[i][3]=output[i][3];
+			arr[i][4]=output[i][4];
+			arr[i][5]=output[i][5];
+
+
 		}
 
 }
 
 // Sort arr[] of size n using Radix Sort.
-void sort(ap_int<5>  arr[9][6])
+void sort(ap_int<2>  arr[18][6])
 {
 #pragma HLS ARRAY_PARTITION variable=arr complete dim=0
 	int exp;
