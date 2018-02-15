1fb36c7c30f0021cdc6cb87a65a17541bd736ad2
vedant8 <vedant8basu@gmail.com>
2018-02-15 22:07:16 +0530

    Add files via upload

---
+++ Bitonic/bitsort.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include<math.h>
+#include <stdint.h>
+int a[8];
+void bitsort();
---
+++ Bitonic/testbitsort.cpp
@@ -0,0 +1,18 @@
+#include"bitsort.h"
+int main ()
+{
+	a [8] = { 16, 24, 72, 40};
+	bitsort(arr);
+		 
+			for(int i=0;i<4;i++)
+				{
+				printf("%d",arr[i]);
+				printf("\n");
+				}
+		
+
+
+
+		
+			return 0;
+}
