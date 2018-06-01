f20faef1055cf5f652bcfcdc5a0e6fbd6012f827
vedant8 <vedant8basu@gmail.com>
2018-06-01 17:43:42 +0200

    Update HCALTower.h

--- HCALTower/HCALTower.h
+++ HCALTower/HCALTower.h
@@ -3,9 +3,7 @@
 #include <stdint.h>
 const uint8_t NCaloLayer1Eta=17;
 const uint8_t NCaloLayer1Phi=4;
-// void getTowerPeaks3x4(uint16_t towerETRegion[3][4],
-//                         uint16_t cEta[5],
-//                         uint16_t cPhi[5]);
-void getTowerPeaks3x4(uint16_t towerETRegions[3][4],uint16_t cEta[5],
-                        uint16_t cPhi[5]);
+
+// void getTowerPeaks3x4(uint16_t towerETRegions[3][4],uint16_t cEta[5],
+                        // uint16_t cPhi[5],uint16_t ClusterDeposits[32], uint16_t ClusterEta[32], uint16_t ClusterPhi[32]);
 void TowerPeaks(uint16_t towerET[17][4], uint16_t TowerPhi[30], uint16_t TowerEta[30]);	 
